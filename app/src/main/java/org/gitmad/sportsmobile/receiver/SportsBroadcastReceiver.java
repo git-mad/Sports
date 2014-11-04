658da94f9ac17f7f8538d9ac1cf879e2cfdc0ac4
Eric Cochran <smartandmellow@gmail.com>
2014-11-04 09:26:44 -0500

    remove duplicate permission in manifest

--- app/src/main/java/org/gitmad/sportsmobile/receiver/SportsBroadcastReceiver.java
+++ app/src/main/java/org/gitmad/sportsmobile/receiver/SportsBroadcastReceiver.java
@@ -3,17 +3,18 @@ package org.gitmad.sportsmobile.receiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.parse.ParseBroadcastReceiver;
 
 public class SportsBroadcastReceiver extends ParseBroadcastReceiver {
 
-    private static final String ACTION ="org.gitmad.sportsmobile.receiver.SportsPushAction";
+    private static final String EXTRA_DATA = "com.parse.Data";
 
     @Override public void onReceive(Context context, Intent intent)
     {
         //Do something interesting here
         Log.d("SportsBroadcastReceiver", "onReceive called.");
-        super.onReceive(context, intent);
+        Toast.makeText(context, intent.getStringExtra(EXTRA_DATA), Toast.LENGTH_LONG).show();
     }
 }
