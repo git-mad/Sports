92bff010bc6bf3491507d1e734668dd94750e590
Eric Cochran <smartandmellow@gmail.com>
2014-11-09 16:29:35 -0500

    init commit for wear sensors

---
+++ wear/src/main/java/org/gitmad/sportswear/GameActivity.java
@@ -0,0 +1,25 @@
+package org.gitmad.sportswear;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class GameActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
